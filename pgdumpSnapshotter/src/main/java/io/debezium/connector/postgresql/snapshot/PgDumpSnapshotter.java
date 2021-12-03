3eea42cf8cb07b948024a56c435ef39d6d695b21
mry <1312348150@qq.com>
2021-12-03 11:02:42 +0800

    fix pgdumpsnapshot

--- pgdumpSnapshotter/src/main/java/io/debezium/connector/postgresql/snapshot/PgDumpSnapshotter.java
+++ pgdumpSnapshotter/src/main/java/io/debezium/connector/postgresql/snapshot/PgDumpSnapshotter.java
@@ -1,8 +1,3 @@
-/*
- * Copyright Debezium Authors.
- *
- * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
- */
 package io.debezium.connector.postgresql.snapshot;
 
 import java.time.Duration;
@@ -19,11 +14,9 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class PgDumpSnapshotter implements Snapshotter {
-    //private final static Logger LOGGER = LoggerFactory.getLogger(PgDumpSnapshotter.class);
     private OffsetState sourceInfo;
     @Override
     public Optional<String> buildSnapshotQuery(TableId tableId) {
-        // DBZ-298 Quoting name in case it has been quoted originally; it doesn't do harm if it hasn't been quoted
         StringBuilder q = new StringBuilder();
         q.append("SELECT * FROM ");
         q.append(tableId.toDoubleQuotedString());
@@ -43,12 +36,12 @@ public class PgDumpSnapshotter implements Snapshotter {
             try {
                 String cmd1="sh ~/pg2og_migration/export.sh"+" "+newSlotInfo.snapshotName();
                 String cmd2="sh ~/pg2og_migration/import.sh";
-		        System.out.println(df.format(new Date())+"  "+"outline migration start...");
+                System.out.println(df.format(new Date())+"  "+"outline migration start...");
                 Process ps = Runtime.getRuntime().exec(new String[] {"/bin/sh","-c",cmd1});
                 ps.waitFor();
                 ps=Runtime.getRuntime().exec(new String[] {"/bin/sh","-c",cmd2});
                 ps.waitFor();
-		        System.out.println(df.format(new Date())+"  "+"outline migration end.");
+                System.out.println(df.format(new Date())+"  "+"outline migration end...");
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -64,23 +57,18 @@ public class PgDumpSnapshotter implements Snapshotter {
 
     @Override
     public boolean shouldStream() {
-        //return false;
         return true;
     }
 
     @Override
     public boolean shouldSnapshot() {
         if (sourceInfo == null) {
-            //LOGGER.info("Taking initial snapshot for new datasource");
             return true;
         }
         else if (sourceInfo.snapshotInEffect()) {
-            //LOGGER.info("Found previous incomplete snapshot");
             return true;
         }
         else {
-            //LOGGER.info(
-            //        "Previous snapshot has completed successfully, streaming logical changes from last known position");
             return false;
         }
     }
